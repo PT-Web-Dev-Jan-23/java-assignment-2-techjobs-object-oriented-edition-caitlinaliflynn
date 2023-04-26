f46d1fbead25aa9b21a33340f175ba75d834100e
Ali <caitlinaliflynn@gmail.com>
2023-04-26 16:19:31 -0500

    finished parts 1-5

--- src/main/java/org/launchcode/techjobs/oo/Job.java
+++ src/main/java/org/launchcode/techjobs/oo/Job.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobs.oo;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Job {
@@ -17,9 +18,97 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    public Job() {
+        id = nextId;
+        nextId++;
+    }
+
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency) {
+        this();
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+
+    }
+
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     //  match.
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Job)) return false;
+        Job job = (Job) o;
+        return getId() == job.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+    @Override
+    public String toString() {
+        StringBuilder testString = new StringBuilder("\n");
+        if (id == 0) {
+            testString.append("ID: " + "Data not available" + "\n");
+        } else {
+            testString.append("ID: ").append(id).append("\n");
+        }
+        testString.append("Name: ").append(Objects.requireNonNullElse(name, "Data not available")).append("\n");
+        for (String s : Arrays.asList("Employer: " + Objects.requireNonNullElse(employer, "Data not available") + "\n", "Location: " + Objects.requireNonNullElse(location, "Data not available") + "\n", "Position Type: " + Objects.requireNonNullElse(positionType, "Data not available") + "\n", "Core Competency: " + Objects.requireNonNullElse(coreCompetency, "Data not available") + "\n")) {
+            testString.append(s);
+        }
+        return testString.toString();
+    }
+
+
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+
+    public void setCoreCompetency(CoreCompetency coreCompetency) {
+        this.coreCompetency = coreCompetency;
+    }
 }
